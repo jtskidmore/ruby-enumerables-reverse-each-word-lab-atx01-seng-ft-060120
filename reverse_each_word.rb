c41712d5765ec315ff1de203856ad27b0418931a
JT Skidmore <johntoddskidmore@gmail.com>
2020-05-08 18:43:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,5 @@
 def reverse_each_word (string)
   
-  new_array = []
   
-  string.each do |element|
-    new_array << element
-  end
-
-  new_array2 = []
-  
-  new_array.each do |element|
-    new_array2 << element.reverse
-  end
-  
-  new_array2.join
   
 end
